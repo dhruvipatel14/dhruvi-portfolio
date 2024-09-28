52a2f16f95f81884e3980e709f12e0843d31258e
Dhruvi Patel <dp7070dp@gmail.com>
2024-09-28 15:24:57 -0400

    updated works for mobile devices, removed tilt from mobiles

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,58 +1,109 @@
 import { motion } from 'framer-motion'
 import { Tilt } from 'react-tilt'
-import { github, link, web } from '../assets'
+import { github, link } from '../assets'
 import { SectionWrapper } from '../hoc'
 import { projects } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 import { styles } from "../style";
+import { useMediaQuery } from 'react-responsive';
+
 
 const ProjectCard = ({ index, name, description, tags, image, source_code_link, website_link }) => {
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+  
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450
-        }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] xs:w-[200px] w-full'
-      >
-        <div className='relative w-full h-[230px]'>
-          <img src={image} alt={name} className='w-full h-full object-cover rounded-2xl' loading="lazy" />
+      {!isMobile ? (
+        <Tilt
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className='bg-tertiary p-5 rounded-2xl sm:w-[360px] xs:w-[200px] w-full'
+        >
 
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover gap-2'>
+          <div className='relative w-full h-[230px]'>
+            <img src={image} alt={name} className='w-full h-full object-cover rounded-2xl' loading="lazy" />
 
-            {
-              website_link && (
-                <div onClick={() => window.open(website_link, "_blank")}
-                  className='white-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
-                  <img src={link} alt="link" className=' object-contain' />
-                </div>
-              )
-            }
+            <div className='absolute inset-0 flex justify-end m-3 card-img_hover gap-2'>
+
+              {
+                website_link && (
+                  <div onClick={() => window.open(website_link, "_blank")}
+                    className='white-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
+                    <img src={link} alt="link" className=' object-contain' />
+                  </div>
+                )
+              }
 
 
-            <div onClick={() => window.open(source_code_link, "_blank")}
-              className='black-gradient  w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
-              <img src={github} alt="github" className='w-1/2 h-1/2 object-contain ' />
+              <div onClick={() => window.open(source_code_link, "_blank")}
+                className='black-gradient  w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
+                <img src={github} alt="github" className='w-1/2 h-1/2 object-contain ' />
+              </div>
             </div>
           </div>
-        </div>
 
 
-        <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
-        </div>
+          <div className='mt-5'>
+            <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+            <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+          </div>
 
-        <div className='mt-4 flex flex-wrap gap-2'>
-          {tags.map((tag) => (
-            <p className={`text-[14px] ${tag.color}`} key={`${name}-${tag.name}`}>
-              #{tag.name}
-            </p>
-          ))}
+          <div className='mt-4 flex flex-wrap gap-2'>
+            {tags.map((tag) => (
+              <p className={`text-[14px] ${tag.color}`} key={`${name}-${tag.name}`}>
+                #{tag.name}
+              </p>
+            ))}
+          </div>
+
+        </Tilt>
+
+      ) : (
+
+        <div className='bg-tertiary p-5 rounded-2xl sm:w-[360px] xs:w-[200px] w-full'>
+
+
+          <div className='relative w-full h-[230px]'>
+            <img src={image} alt={name} className='w-full h-full object-cover rounded-2xl' loading="lazy" />
+
+            <div className='absolute inset-0 flex justify-end m-3 card-img_hover gap-2'>
+
+              {
+                website_link && (
+                  <div onClick={() => window.open(website_link, "_blank")}
+                    className='white-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
+                    <img src={link} alt="link" className=' object-contain' />
+                  </div>
+                )
+              }
+
+
+              <div onClick={() => window.open(source_code_link, "_blank")}
+                className='black-gradient  w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
+                <img src={github} alt="github" className='w-1/2 h-1/2 object-contain ' />
+              </div>
+            </div>
+          </div>
+
+
+          <div className='mt-5'>
+            <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+            <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+          </div>
+
+          <div className='mt-4 flex flex-wrap gap-2'>
+            {tags.map((tag) => (
+              <p className={`text-[14px] ${tag.color}`} key={`${name}-${tag.name}`}>
+                #{tag.name}
+              </p>
+            ))}
+          </div>
         </div>
-      </Tilt>
+      )
+      }
     </motion.div>
   )
 }
@@ -70,12 +121,12 @@ const Works = () => {
 
       <div className='w-full flex'>
         <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-justify text-[17px] leading-[30px]'
+          variants={fadeIn("", "", 0.1, 1.5)}
+          className='mt-3 text-secondary text-justify xs:text-[15px] xs:leading-[15px] sm:text-[17px] sm:leading-[30px] text-[17px] leading-[30px]'
         >
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in i  t. It reflects my
+          links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
         </motion.p>
