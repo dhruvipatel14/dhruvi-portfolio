03dea7c1d2339a4bbe3cdc9f3ed8d3bf75ac1eb6
Dhruvi Patel <dp7070dp@gmail.com>
2024-09-28 16:07:19 -0400

    added ProjectMobileCard

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,13 +7,54 @@ import { fadeIn, textVariant } from '../utils/motion'
 import { styles } from "../style";
 import { useMediaQuery } from 'react-responsive';
 
+const ProjectCardMobile = ({ index, name, description, tags, image, source_code_link, website_link }) => {
+  return (
+    <>
+      <div className='bg-tertiary p-5 rounded-2xl sm:w-[360px] xs:w-[200px] w-full'>
+        <div className='relative w-full h-[230px]'>
+          <img src={image} alt={name} className='w-full h-full object-cover rounded-2xl' loading="lazy" />
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover gap-2'>
+
+            {
+              website_link && (
+                <div onClick={() => window.open(website_link, "_blank")}
+                  className='white-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
+                  <img src={link} alt="link" className=' object-contain' />
+                </div>
+              )
+            }
+
+
+            <div onClick={() => window.open(source_code_link, "_blank")}
+              className='black-gradient  w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
+              <img src={github} alt="github" className='w-1/2 h-1/2 object-contain ' />
+            </div>
+          </div>
+        </div>
+
+
+        <div className='mt-5'>
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        </div>
+
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => (
+            <p className={`text-[14px] ${tag.color}`} key={`${name}-${tag.name}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
 
 const ProjectCard = ({ index, name, description, tags, image, source_code_link, website_link }) => {
-  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
-  
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      {!isMobile ? (
+    <>
+      <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
         <Tilt
           options={{
             max: 45,
@@ -45,7 +86,6 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link, 
             </div>
           </div>
 
-
           <div className='mt-5'>
             <h3 className='text-white font-bold text-[24px]'>{name}</h3>
             <p className='mt-2 text-secondary text-[14px]'>{description}</p>
@@ -58,92 +98,85 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link, 
               </p>
             ))}
           </div>
-
         </Tilt>
-
-      ) : (
-
-        <div className='bg-tertiary p-5 rounded-2xl sm:w-[360px] xs:w-[200px] w-full'>
-
-
-          <div className='relative w-full h-[230px]'>
-            <img src={image} alt={name} className='w-full h-full object-cover rounded-2xl' loading="lazy" />
-
-            <div className='absolute inset-0 flex justify-end m-3 card-img_hover gap-2'>
-
-              {
-                website_link && (
-                  <div onClick={() => window.open(website_link, "_blank")}
-                    className='white-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
-                    <img src={link} alt="link" className=' object-contain' />
-                  </div>
-                )
-              }
-
-
-              <div onClick={() => window.open(source_code_link, "_blank")}
-                className='black-gradient  w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
-                <img src={github} alt="github" className='w-1/2 h-1/2 object-contain ' />
-              </div>
-            </div>
-          </div>
-
-
-          <div className='mt-5'>
-            <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-            <p className='mt-2 text-secondary text-[14px]'>{description}</p>
-          </div>
-
-          <div className='mt-4 flex flex-wrap gap-2'>
-            {tags.map((tag) => (
-              <p className={`text-[14px] ${tag.color}`} key={`${name}-${tag.name}`}>
-                #{tag.name}
-              </p>
-            ))}
-          </div>
-        </div>
-      )
-      }
-    </motion.div>
+      </motion.div>
+    </>
   )
 }
 const Works = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-center`}>
-          My Work
-        </p>
-        <h2 className={`${styles.sectionHeadText} text-center`}>
-          Projects.
-        </h2>
-      </motion.div>
+      {!isMobile ?
+        (
+          <>
+            <motion.div variants={textVariant()}>
+              <p className={`${styles.sectionSubText} text-center`}>
+                My Work
+              </p>
+              <h2 className={`${styles.sectionHeadText} text-center`}>
+                Projects.
+              </h2>
+            </motion.div>
 
-      <div className='w-full flex'>
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1.5)}
-          className='mt-3 text-secondary text-justify xs:text-[15px] xs:leading-[15px] sm:text-[17px] sm:leading-[30px] text-[17px] leading-[30px]'
-        >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
-        </motion.p>
-      </div>
+            <div className='w-full flex'>
+              <motion.p
+                variants={fadeIn("", "", 0.1, 1.5)}
+                className='mt-3 text-secondary text-justify xs:text-[15px] xs:leading-[15px] sm:text-[17px] sm:leading-[30px] text-[17px] leading-[30px]'
+              >
+                Following projects showcases my skills and experience through
+                real-world examples of my work. Each project is briefly described with
+                links to code repositories and live demos in it. It reflects my
+                ability to solve complex problems, work with different technologies,
+                and manage projects effectively.
+              </motion.p>
+            </div>
+          </>
+
+        ) :
+        (
+          <>
+            <div>
+              <p className={`${styles.sectionSubText} text-center`}>
+                My Work
+              </p>
+              <h2 className={`${styles.sectionHeadText} text-center`}>
+                Projects.
+              </h2>
+            </div>
+
+            <div className='w-full flex'>
+              <p
+                className='mt-3 text-secondary text-justify xs:text-[15px] xs:leading-[15px] sm:text-[17px] sm:leading-[30px] text-[17px] leading-[30px]'
+              >
+                Following projects showcases my skills and experience through
+                real-world examples of my work. Each project is briefly described with
+                links to code repositories and live demos in it. It reflects my
+                ability to solve complex problems, work with different technologies,
+                and manage projects effectively.
+              </p>
+            </div>
+          </>
+        )}
 
       <div className='mt-20 flex flex-wrap gap-7'>
         {
           projects.map((project, index) => (
-            <ProjectCard key={`project-${index}`}
-              index={index}
-              {...project}
-            />
+
+            isMobile ?
+              (<ProjectCardMobile key={`project-${index}`}
+                index={index}
+                {...project} />) :
+              (<ProjectCard key={`project-${index}`}
+                index={index}
+                {...project} />)
           ))
         }
       </div>
     </>
   )
 }
+
 
 export default SectionWrapper(Works, "") 
